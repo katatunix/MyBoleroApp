1d7417312372cf58c704c900eb0edf2e5db2a0b9
katatunix <katatunix@gmail.com>
2024-12-08 00:49:16 +0700

    ok good

---
+++ clean.sh
@@ -0,0 +1,4 @@
+#!/bin/sh -x
+rm -dfr bin
+rm -dfr obj
+rm -dfr dist
--- publish.sh
+++ publish.sh
@@ -1,6 +1,4 @@
 #!/bin/sh -x
-dotnet clean
-rm -dfr bin
-rm -dfr obj
-rm -dfr dist
-dotnet publish -o dist # -p:GHPages=true
+
+./clean.sh
+dotnet publish -o dist
